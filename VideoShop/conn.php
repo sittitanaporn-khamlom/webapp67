e105891f94f72392ab47fbdbdd15e0433bbfc6ea
sittitanaporn-khamlom <sittitanaporn@gmail.com>
2024-09-26 15:25:58 +0700

    Delete VideoShop directory

--- VideoShop/conn.php
+++
@@ -1,6 +0,0 @@
-<?php
-$conn = new mysqli('localhost','root','','videoshop');
-$conn->query("SET NAMES utf8");
-if($conn->connect_error){
-    die("Connection Fail God damn it ". $conn->$conn_error);
-}
